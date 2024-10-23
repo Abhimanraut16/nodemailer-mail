7cc8eb4035deb806f5a21fa4294b1c0397af9e36
Abhimanraut16 <rautabhimanu58@gmail.com>
2024-10-23 16:15:48 +0545

    email

---
+++ evn-local.js
@@ -0,0 +1,11 @@
+// NEXT_PUBLIC_ADMIN_EMAIL="abhimanurauttharu@gmail.com"
+// NEXT_PUBLIC_ADMIN_EMAIL_FULL="Test Semder <abhimanurauttharu@gmail.com>"
+// NEXT_PUBLIC_EMAIL_PASSWORD="ishbitckwmkoures"
+// NEXT_PUBLIC_EMAIL_HOST="smtp.gmail.com"
+// NEXT_PUBLIC_EMAIL_PORT=465
+// NEXT_PUBLIC_PERSONAL_EMAIL="abhimanurauttharu@gmail.com"
+
+// # NEXT_PUBLIC_PERSONAL_EMAIL="kushal.giri@omegabpo.com"
+
+// create the .env.local file ,
+// this code used to .env.local file under
